aa28070bb4f546b384e8e7850ebbee538c6689bc
goldenst <jeff@goldenstatetow.com>
2021-06-25 11:57:29 -0700

    first commit

---
+++ Faler-backend/fallen-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'beced5cb2ae2590243845bf179446406'),
+    },
+  },
+});
---
+++ Faler-backend/fallen-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '843411e7-ee37-4f17-980a-f117f6265653'
+};
